3effe668e1339db64c46f9000f98c13d17467638
Umit CEYLAN <byildiz2019@gmail.com>
2023-11-10 15:28:56 +0100

    Update serverbuild.ps1

--- serverbuild.ps1
+++ serverbuild.ps1
@@ -18,11 +18,11 @@ $webclient.DownloadFile($regionalsettingsURL,$RegionalSettings)
 & $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$RegionalSettings`""
 
 # Set languages/culture. Not needed perse.
-Set-WinSystemLocale en-AU
-Set-WinUserLanguageList -LanguageList en-AU -Force
-Set-Culture -CultureInfo en-AU
+Set-WinSystemLocale nb-NO
+Set-WinUserLanguageList -LanguageList nb-NO -Force
+Set-Culture -CultureInfo nb-NO
 Set-WinHomeLocation -GeoId 12
-Set-TimeZone -Name "E. Australia Standard Time"
+Set-TimeZone -Name "W. Europe Standard Time"
 
 # restart virtual machine to apply regional settings to current user. You could also do a logoff and login.
 Start-sleep -Seconds 40
